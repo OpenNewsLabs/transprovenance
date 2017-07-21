e27213ee09df93e8474fd2c93ac8bdbad314a963
Adam Arling <adam.arling@northwestern.edu>
2017-07-21 12:26:24 -0400

    Update timeline click handler

--- js/custom/main.js
+++ js/custom/main.js
@@ -45,6 +45,21 @@ $(document).ready( function() {
 	});
 
 	loadTranscriptText();
+
+	hyperaudiolite.init('transcript', 'video');
+
+	$('.timelineContainer').click(function() {
+		var currentTime = $('#video')[0].currentTime,
+			displayVid1 = (currentTime < 35) || (currentTime > 62 && currentTime < 77) || (currentTime > 122),
+			displayVid2 = (currentTime > 35 && currentTime < 62) || (currentTime > 77 && currentTime < 122);
+
+		if (displayVid1) {
+			alert('Video #1');
+		} else if (displayVid2) {
+			alert('Video #2');
+		}
+	});
+
 });
 
 
